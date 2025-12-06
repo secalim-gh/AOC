ffa0eed1cd0c17196e4919bfec53140b91fb4521
secalim <secalim@proton.me>
2025-12-06 16:37:12 +0100

    Refined Part 1

--- 2025/06/p1/main.swift
+++ 2025/06/p1/main.swift
@@ -1,42 +1,35 @@
 import Foundation
 
-func calc(current: Int, op: Character, value: String) -> Int {
-  guard let number = Int(value) else { return current }
-  switch op {
-    case "+":
-      return current + number
-    case "-":
-      return current - number
-    case "*":
-      return current * number
-    default:
-      return current
-  }
-}
-
 let arguments = CommandLine.arguments
 
 if arguments.count < 2 {
-  print("Error: Missing filename argument.")
+	print("Error: Missing filename argument.")
 } else {
-  let path = arguments[1]
-  let content = try String(contentsOfFile: path, encoding: .utf8)
+	let path = arguments[1]
+	let content = try String(contentsOfFile: path, encoding: .utf8)
 
-  var c = content.split(separator: "\n")
-  let operators = c.popLast()!.replacingOccurrences(of: " ", with: "")
-  var lines = c.map {
-    $0.split(separator: " ").map { String($0) }
-  }
+	var c = content.split(separator: "\n")
+	let operators = c.popLast()!.replacingOccurrences(of: " ", with: "")
+	let lines = c.map {
+		$0.split(separator: " ").map { Int($0)! }
+	}
 
-  let firstLine = lines.removeFirst()
-  var results: [Int] = firstLine.map { Int($0)! }
-  
-  for (i, op) in operators.enumerated() {
-    for line in lines {
-      let number = line[i]
-      results[i] = calc(current: results[i], op: op, value: number)
-    }
-  }
-  let result = results.reduce(0, +)
-  print(result)
+	let columnCount = lines[0].count
+	let rotated = (0..<columnCount).map { i in
+		lines.map { $0[i] }
+	}
+	
+	var results: [Int] = []
+	for (i, op) in operators.enumerated() {
+		switch op {
+			case "+":
+				results.append(rotated[i].reduce(0, +))
+			case "*":
+				results.append(rotated[i].reduce(1, *))
+			default:
+				print("Unexpected operator")
+		}
+	}
+	let result = results.reduce(0, +)
+	print(result)
 }
